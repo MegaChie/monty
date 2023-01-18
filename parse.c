9b786e7fa4c69dbd39022c1e884ba78c00e0f4a3
MegaChie <workspace.mega@gmail.com>
2023-01-18 19:16:27 +0200

    Create parse.c

---
+++ parse.c
@@ -0,0 +1,58 @@
+#include "monty.h"
+/**
+ * 
+ */
+void parseLine(line_t *line, char *buffer)
+{
+	unsigned int count;
+	char *token = NULL;
+
+	line->content = malloc(sizeof(char *) * 3);
+	if (line->content == 0)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(buffer, " '\n");
+	for (count = 0; token && count < 2; count++)
+	{
+		line->content[count] = token;
+		token = strtok(NULL, " \n");
+	}
+	line->content[count] = NULL;
+}
+
+/**
+ * 
+ */
+void parseFile(FILE *file)
+{
+	size_t size = 0;
+	meta_t *meta = NULL;
+	line_t line;
+
+	meta = malloc(sizeof(meta_t));
+	if (meta == 0)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	line.number = 0;
+	line.content = NULL;
+	meta->file = file;
+	meta->stack = NULL;
+	meta->buffer = NULL;
+	while (getLine(&(meta->buffer), &size, meta->file) != -1)
+	{
+		line.number++
+		parseLine(&line, meta->buffer)
+		if (line.content)
+		{
+			get_op_func(line, meta)(&(meta->stack), line.number);
+		}
+	}
+	free(meta->buffer);
+	freeStack(&(meta->stack));
+	fclose(meta->file);
+	free(meta);
+}
